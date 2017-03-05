d3a27131249200bf447dea03781b4d6cb782ce8e
zhangjianchun <113274179@qq.com>
2017-03-05 21:37:59 +0800

    ng 服务器 的优化，和bootstrap几小模块加入！

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,12 +1,20 @@
 'use strict';
 
 angular.module('zjcApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma',
-      'MainCtrl-MainCtrl-MainCtrlMainCtrl-MainCtrl-MainCtrl'
-    ];
-  });
-console.log('main.js');
+    .controller('MainCtrl', function(Zjcservice, $scope) {
+        $scope.awesomeThings = [
+            'HTML5 Boilerplate',
+            'AngularJS',
+            'Karma',
+            'MainCtrl-MainCtrl-MainCtrlMainCtrl-MainCtrl-MainCtrl'
+        ];
+
+      Zjcservice.GET('api/json').success(function(data){
+      	$scope.box=data;
+      })
+ 
+         Zjcservice.JSONP('.cn').success(function(data) {
+            $scope.events =data
+        });
+        
+    });
--- app/scripts/services/zjcservice.js
+++ app/scripts/services/zjcservice.js
@@ -1,6 +1,45 @@
 'use strict';
 
 angular.module('zjcApp')
-  .service('Zjcservice', function Zjcservice() {
-    // AngularJS will instantiate a singleton by calling "new" on this function
-  });
+    // .service('Zjcservice', function Zjcservice() {
+    .service('Zjcservice', function Zjcservice($http) {
+        // AngularJS will instantiate a singleton by calling "new" on this function
+           var githubUrl = 'mock.Datag';
+        var runUserRequest = function(username) {
+            // 从使用JSONP调用Github API的$http服务中返回promise
+            return $http({
+                method: 'GET',
+                url: githubUrl + 
+                    username //+'?callback=JSON_CALLBACK'
+            });
+        };
+        return {
+            JSONP: function(username) {
+                return runUserRequest(username)
+            },
+            GET: function(a) {
+                
+                return $http.get(a)
+            }
+        }
+    });
+//   
+
+// angular.module('zjcApp')
+//     .factory('Zjcservice', function($http) {
+//         var githubUrl = 'api/json';
+//         var runUserRequest = function(username, path) {
+//             // 从使用JSONP调用Github API的$http服务中返回promise
+//             return $http({
+//                 method: 'git',
+//                 url: githubUrl 
+//             });
+//         };
+//          // 返回带有一个events函数的服务对象
+//         return {
+//             events: function(username) {
+//                 return runUserRequest(username, 'events');
+
+//             }
+//         };
+//     });
